4a79e1f0e3f52b39cc3659ca339ea8c2fda3c6b8
Markus N. <markus.n@gmx.net>
2015-06-14 18:39:44 +0200

    initial, simple test for testGetContentFromAmazon

--- DataCollectorTest/test/connection/RequestConnectorTest.java
+++ DataCollectorTest/test/connection/RequestConnectorTest.java
@@ -1,5 +1,34 @@
 package connection;
 
+import static org.junit.Assert.assertNotNull;
+
+import java.util.HashMap;
+
+import org.junit.Test;
+
+import requestAgents.GenericAgent;
+import requestAgents.RequestAgent;
+
+// test GetContentFromAmazon method
+
 public class RequestConnectorTest {
-//------------test evaluateXPath
+	@Test
+	public void testGetContentFromAmazon(){
+		
+		// create new requestConnector
+		RequestConnector requestConnector = new RequestConnector();
+		// Response should be a Hashmap
+		HashMap<String,String> response = new HashMap<String,String>();
+		
+		// create GenericAgent
+		String useragent ="Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:32.0) Gecko/20100101 Firefox/32.0";
+		int kindOFDevice = RequestAgent.normal;
+		GenericAgent agent = new GenericAgent("bla",useragent , kindOFDevice,1920,1080);
+		
+		// send request to test method
+		response = requestConnector.GetContentFromAmazon("http://sqat.eu-gb.mybluemix.net", "Test", agent);
+		
+		// is response empty?
+		assertNotNull(response);
+	}
 }
